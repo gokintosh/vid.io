b77252a78415bb2eb62a594e8fb93d19a76caff1
gokintosh <gokul.kannambally_balachandran@student.po.edu.pl>
2021-07-23 21:13:44 +0200

    initial commi

---
+++ backend/src/main/java/com/gokul/vid_io/VidIoApplication.java
@@ -0,0 +1,13 @@
+package com.gokul.vid_io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidIoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VidIoApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/gokul/vid_io/VidIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokul.vid_io;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidIoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
